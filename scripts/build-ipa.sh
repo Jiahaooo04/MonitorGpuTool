76dbd537e94210bb54987fb8bf6b2e32bbdcdfae
Taropaste <140699085+Jiahaooo04@users.noreply.github.com>
2026-08-17 16:07:17 +0800

    Add files via upload

--- scripts/build-ipa.sh
+++ scripts/build-ipa.sh
@@ -35,8 +35,8 @@ cd ios
 pod install --repo-update
 cd ..
 
-echo "==> Building Flutter iOS Archive (Release)..."
-flutter build ipa --release --no-codesign
+echo "==> Building Flutter iOS App (Release)..."
+flutter build ios --release --no-codesign
 
 echo "==> Packaging into RunMon.ipa..."
 APP_PATH="build/ios/archive/Runner.xcarchive/Products/Applications/Runner.app"
